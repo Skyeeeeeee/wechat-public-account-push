bb34a65f0d1158284545603784b7f994d12ff95d
Skyeeeeeee <58503777+Skyeeeeeee@users.noreply.github.com>
2022-12-18 18:18:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -17,17 +17,16 @@ const USER_CONFIG = {
 
   PROVINCE: '内蒙古',
   CITY: '呼伦贝尔',
-  
-  openURL:"www.baidu.com",
+
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '苏姐姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '94ad205b68304c40b77ae8ee027f0695',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -55,13 +54,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oaHh_55N-z0iohmWbcNpnegZabLA',
+      id: '',
     }
   ],
 
